67a539f558172542fdc6459b9d509c3e2dab864a
samuelhoppenworth <shopp7@illinois.edu>
2025-02-20 14:02:10 -0600

    refactored for separation of concerns

--- game_engine.py
+++ game_engine.py
@@ -1,4 +1,4 @@
-import os
+from time import sleep
 
 class Ship:
     def __init__(self, name, length, num_available):
@@ -48,7 +48,7 @@ class Player:
         self.id = id
         self.hits_received = 0
         self.player_board = [["-" for _ in range(10)] for _ in range(10)]
-        self.opponent_board = [["-" for _ in range(10)] for _ in range(10)]
+        self.hits_misses = [["-" for _ in range(10)] for _ in range(10)]
         self.inventory = {
             "A" : Ship(name="Aircraft_Carrier",length=5, num_available=1),
             "B" : Ship(name="Battleship", length=4, num_available=1),
@@ -66,8 +66,8 @@ class Player:
     def get_player_board(self):
         return self.player_board
     
-    def get_opponent_board(self):
-        return self.opponent_board
+    def get_hits_misses(self):
+        return self.hits_misses
         
     def get_inventory(self):
         return self.inventory
@@ -76,7 +76,6 @@ class Player:
         return self.inventory[ship_name]
 
     def ship_available(self, placement: Placement):
-        """Returns true if a ship is available to be placed"""
         ship_name = placement.get_ship_name()
         return (self.inventory[ship_name].num_available > 0)
         
@@ -101,25 +100,21 @@ class Player:
             else:
                 self.player_board[x][y + i] = '1'
         self.inventory[ship_name].num_available -= 1
-        
-    def receive_hit(self, x, y):
-        """Marks hit on self.player_board"""
-        self.player_board[x][y] = "X"
-    
+            
     def inc_hits(self):
         self.hits_received += 1
     
     def attack(self, opponent: 'Player', x, y):
-        """Marks hits on the player's representation of opponents board
-        and the opponents representation of their own board. Marks misses 
-        on only the player's representation of the opponents board"""
+        """Marks attack on (x,y) as hit or miss
+        Returns True on hit, False on miss"""
         opponent_placements = opponent.get_player_board()
-        if opponent_placements[x][y] == '1':
+        if opponent_placements[x][y] == '1' and self.hits_misses[x][y] == '-':
             opponent.inc_hits()
-            opponent.receive_hit(x, y)
-            self.opponent_board[x][y] = "X"
+            self.hits_misses[x][y] = "X"
+            return True
         elif opponent_placements[x][y] == '-':
-            self.opponent_board[x][y] = "O"
+            self.hits_misses[x][y] = "O"
+        return False
 
                 
 class Engine:
@@ -129,57 +124,20 @@ class Engine:
         self.player1 = Player(id=1)
         self.player2 = Player(id=2)
      
-    def get_player(self, id): # TODO: input error checking
-         return self.player1 if id == 1 else self.player2
+    def get_player(self, id):
+         return self.player1 if id == 1 else self.player2 if id == 2 else None
      
     def is_player_turn(self, id):
-        return self.player1_turn if id == 1 else not self.player1_turn
+        return self.player1_turn if id == 1 else not self.player1_turn if id == 2 else None
     
     def pass_turn(self):
         self.player1_turn = not self.player1_turn
-    
-    def check_ship_name(self, user_ship):
-        SHIP_SYMBOLS = {"A", "B", "C", "D", "S"}
-        return user_ship in SHIP_SYMBOLS
-    
-    def check_orientation(self, user_orientation):
-        ORIENTATIONS = {"V", "H"}
-        return user_orientation in ORIENTATIONS
-
-    def check_coord_format(self, user_coords):
-        """"Ensures coordinate input can be converted to ASCII"""
-        if len(user_coords) != 2 and len(user_coords) != 3:
-            return False
-        if not user_coords[0].isalpha() or not user_coords[1:].isnumeric(): 
-            return False
-        return True
-
-    def check_input_format(self, user_input):
-        """Runs all checks on input format, but not necessarily input legality"""
-        args = user_input.split()
-        if len(args) != 3:
-            return False
-        ship_name, orientation, user_coords = args
-        return (self.check_ship_name(ship_name) and self.check_orientation(orientation) and self.check_coord_format(user_coords))
-
-    def convert_coordinates(self, user_input):
-        """Converts coordinate input to indices of a 2D array of shape (10, 10)"""
-        internal_x = ord(user_input[0]) - ord('A')
-        internal_y = int(user_input[1]) - 1 if len(user_input) == 2 else int(user_input[1:]) - 1
-        return internal_x, internal_y
-    
+        
     def check_coord_bounds(self, x, y):
         """Checks that coordinates, but not necessarily placement, are within bounds"""
         lower_bound, upper_bound = 0, 9
         return (lower_bound <= x <= upper_bound) and (lower_bound <= y <= upper_bound)
-    
-    def get_placement(self, user_input: str):
-        """Encapsulates all placement information in Placement object"""
-        ship_name, orientation, user_coords = user_input.split()
-        internal_x, internal_y = self.convert_coordinates(user_coords)
-        ship_length = self.player1.get_ship_data(ship_name).get_length()
-        return Placement(name=ship_name, x=internal_x, y=internal_y, length=ship_length, orientation=orientation)
-    
+        
     def placement_in_bounds(self, placement: Placement):
         """Checks ship remains within bounds when placed"""
         _, x, y, length, orientation = placement
@@ -189,7 +147,7 @@ class Engine:
             return False
         return True
 
-    def valid_placement(self, placement, player: Player):
+    def valid_placement(self, placement: Placement, player: Player):
         """Ensures a placement is legal"""
         return (
             self.placement_in_bounds(placement) and 
@@ -198,6 +156,6 @@ class Engine:
         )
             
     def check_game_end(self):
-        """Returns true when a player has been hit HITS_POSSIBLE number of times"""
-        HITS_POSSIBLE = 15 # Sum of lengths of each ship in a player's inventory
+        """Returns true when all of a player's ships have been sunk"""
+        HITS_POSSIBLE = 18 # Sum of lengths of each ship in a player's inventory
         return (self.player1.get_hits_received() == HITS_POSSIBLE) or (self.player2.get_hits_received() == HITS_POSSIBLE)
